f899520dd0e21996711e4af44dee9773491aec7b
junho <junhoL1046@gmail.com>
2023-05-25 20:15:56 +0900

    feat: TrendingViewController Trending 조회 기능 구현

--- WhereCanISeeThis/Sources/Views/TrendingView/TrendingViewController.swift
+++ WhereCanISeeThis/Sources/Views/TrendingView/TrendingViewController.swift
@@ -2,13 +2,18 @@ import UIKit
 
 final class TrendingViewController: UICollectionViewController {
     private let trendingViewModel: TrendingViewModel
+    private var dataSource: DataSource?
 
     init(trendingViewModel: TrendingViewModel = TrendingViewModel()) {
         self.trendingViewModel = trendingViewModel
-        var configuration = UICollectionLayoutListConfiguration(appearance: .grouped)
+
+        var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
         configuration.backgroundColor = Constants.collectionViewBackgroundColor
+        configuration.headerMode = .firstItemInSection
         let layout = UICollectionViewCompositionalLayout.list(using: configuration)
         super.init(collectionViewLayout: layout)
+
+        collectionView.isScrollEnabled = false
     }
 
     required init?(coder: NSCoder) {
@@ -18,6 +23,82 @@ final class TrendingViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Constants.viewBackgroundColor
+
+        configureDataSource()
+        trendingViewModel.bind { errorMessage in
+            print(errorMessage)
+        }
+        trendingViewModel.bind { [weak self] in
+            guard let self else { return }
+            updateSnapshot()
+        }
+
+        trendingViewModel.action(.fetchMovieGenresList)
+        trendingViewModel.action(.fetchTVShowGenresList)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        trendingViewModel.action(.fetchTrendingMovies)
+        trendingViewModel.action(.fetchTrendingTVShows)
+    }
+
+    func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        let headerRegistration = UICollectionView.CellRegistration(handler: headerRegistrationHandler)
+        dataSource = DataSource(
+            collectionView: collectionView,
+            cellProvider: { collectionView, indexPath, itemIdentifier in
+                switch itemIdentifier {
+                case .header:
+                    return collectionView.dequeueConfiguredReusableCell(
+                        using: headerRegistration, for: indexPath, item: itemIdentifier
+                    )
+                case .media:
+                    return collectionView.dequeueConfiguredReusableCell(
+                        using: cellRegistration, for: indexPath, item: itemIdentifier
+                    )
+                }
+            }
+        )
+    }
+}
+
+extension TrendingViewController {
+    enum Row: Hashable {
+        case header(MediaType)
+        case media(MediaType)
+    }
+
+    typealias DataSource = UICollectionViewDiffableDataSource<MediaType, Row>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<MediaType, Row>
+
+    func cellRegistrationHandler(cell: UICollectionViewCell, indexPath: IndexPath, itemIdentifier: Row) {
+        guard case .media(let mediaType) = itemIdentifier else { return }
+        var contentConfiguration = cell.mediaCollectionContentView(viewModel: trendingViewModel)
+        contentConfiguration.mediaType = mediaType
+        contentConfiguration.itemIDs = trendingViewModel.mediaItemIDs(type: mediaType)
+        cell.contentConfiguration = contentConfiguration
+    }
+
+    func headerRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: Row) {
+        guard case .header(let mediaType) = itemIdentifier else { return }
+        var contentConfiguration = cell.defaultContentConfiguration()
+        let attributedTitle = NSAttributedString(
+            string: mediaType.trendingTitle, attributes: [.font: UIFont.preferredFont(forTextStyle: .headline)]
+        )
+        contentConfiguration.attributedText = attributedTitle
+        cell.contentConfiguration = contentConfiguration
+    }
+
+    private func updateSnapshot() {
+        var snapShot = Snapshot()
+        snapShot.appendSections(MediaType.allCases)
+        MediaType.allCases.forEach { mediaType in
+            snapShot.appendItems([.header(mediaType), .media(mediaType)], toSection: mediaType)
+        }
+        dataSource?.apply(snapShot)
     }
 }
 
