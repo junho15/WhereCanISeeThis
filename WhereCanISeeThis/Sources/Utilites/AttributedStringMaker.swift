11c1685b50032271097ca50fd997b02e2a357df3
junho <junhoL1046@gmail.com>
2023-05-27 15:33:38 +0900

    feat: searchListHeader 케이스 추가

--- WhereCanISeeThis/Sources/Utilites/AttributedStringMaker.swift
+++ WhereCanISeeThis/Sources/Utilites/AttributedStringMaker.swift
@@ -1,20 +1,31 @@
 import UIKit
 
 enum AttributedStringMaker {
-    case trending(title: String)
+    case trendingHeader(title: String)
+    case searchListHeader(title: String, count: Int)
 
     var attributedString: NSAttributedString {
         switch self {
-        case .trending(let title):
-            return trendingAttributedString(title: title)
+        case .trendingHeader(let title):
+            return trendingHeaderAttributedString(title: title)
+        case .searchListHeader(let title, let count):
+            return searchListHeader(title: title, count: count)
         }
     }
 
-    func trendingAttributedString(title: String) -> NSAttributedString {
+    private func trendingHeaderAttributedString(title: String) -> NSAttributedString {
         return NSAttributedString(
             string: title,
             attributes: [.font: UIFont.preferredFont(forTextStyle: .title3),
                          .foregroundColor: UIColor.systemPink]
+        )
+    }
+
+    private func searchListHeader(title: String, count: Int) -> NSAttributedString {
+        return NSAttributedString(
+            string: "\(title) (\(count))",
+            attributes: [.font: UIFont.preferredFont(forTextStyle: .title3),
+                         .foregroundColor: UIColor.systemBlue]
         )
     }
 }
