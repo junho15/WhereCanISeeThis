180b3acb9661da01b6f6f8d3f86c7f7b737a55d6
junho <junhoL1046@gmail.com>
2023-05-31 16:09:59 +0900

    chore: Bundle ID 수정

--- MovieDatabaseAPI/Package.swift
+++ MovieDatabaseAPI/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MovieDatabaseAPI",
     platforms: [
-        .iOS(.v16), .macOS(.v13)
+        .iOS(.v13)
     ],
     products: [
         .library(
