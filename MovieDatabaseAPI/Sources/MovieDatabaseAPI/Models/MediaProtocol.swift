396efb32a0d7c599b791e24b9059ae5e31d6d9ba
junho <junhoL1046@gmail.com>
2023-06-01 15:37:14 +0900

    feat: backdropPath 프로퍼티 요구사항 추가

--- MovieDatabaseAPI/Sources/MovieDatabaseAPI/Models/MediaProtocol.swift
+++ MovieDatabaseAPI/Sources/MovieDatabaseAPI/Models/MediaProtocol.swift
@@ -4,6 +4,7 @@ public protocol MediaProtocol: Identifiable, Decodable {
     var id: Int { get }
     var mediaType: MediaType { get }
     var posterPath: String? { get }
+    var backdropPath: String? { get }
     var title: String? { get }
     var genreIds: [Int]? { get }
     var date: Date? { get }
